768db32df8f11f3560f942671ca6e23db1786051
KurvaAkhileshkumar <81473345+KurvaAkhileshkumar@users.noreply.github.com>
2022-12-13 23:15:52 +0530

    Add files via upload

---
+++ interface2.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+KNNModel=pickle.load(open('knnclassifier.pkl','rb'))
+LRegressionmodel=pickle.load(open('Regressionclassifier.pkl','rb'))
+vectorizer=pickle.load(open('Vectorizer.pickle','rb'))
+
+def emailPrediction(input_data):
+    input_data=[input_data]    
+    email_feautures=vectorizer.transform(input_data)
+    ans=KNNModel.predict(email_feautures)
+    ans2=LRegressionmodel.predict(email_feautures)
+
+    return ans
+
+def main():
+
+    st.set_page_config(page_title="Be Cautious",page_icon=":imp:")
+    st.title('e-mail Spam Detection web app')
+    EnterTheSubject=st.text_input('Subject')
+    EnterTheBody=st.text_input('Body')
+    EnterTheMail=EnterTheSubject + EnterTheBody
+    #code for prediction
+    prediction=''
+    
+    #creating a button for prediction
+    if st.button('Email detection'):
+        prediction=emailPrediction(EnterTheMail)
+    st.success(prediction)
+
+if __name__ == "__main__":
+    main()
