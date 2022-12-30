dd1dcebc53bb0474d816f22d0552549eda9e49c0
KurvaAkhileshkumar <81473345+KurvaAkhileshkumar@users.noreply.github.com>
2022-12-31 02:03:01 +0530

    Update interface2.py

--- interface2.py
+++ interface2.py
@@ -2,20 +2,28 @@ import streamlit as st
 import pickle
 
 KNNModel=pickle.load(open('knnclassifier.pkl','rb'))
-LRegressionmodel=pickle.load(open('Regressionclassifier.pkl','rb'))
+LRegressionModel=pickle.load(open('Regressionclassifier.pkl','rb'))
+NBModel=pickle.load(open('NBclassifier.pkl','rb'))
 vectorizer=pickle.load(open('Vectorizer.pickle','rb'))
 
-def emailPrediction(input_data):
+def Prediction(input_data,option):
     input_data=[input_data]    
     email_feautures=vectorizer.transform(input_data)
-    ans=KNNModel.predict(email_feautures)
-    ans2=LRegressionmodel.predict(email_feautures)
+    ans=''
+    if option=='KNN':
+        ans=KNNModel.predict(email_feautures)
+    elif option=='Naive-Bayes':
+        ans=NBModel.predict(email_feautures)
+    elif option=='Logistic Regression':
+        ans=LRegressionModel.predict(email_feautures)
 
     return ans
 
 def main():
-
     st.set_page_config(page_title="Be Cautious",page_icon=":imp:")
+    option = st.selectbox(
+    'Algorithm',
+    ('Select','Naive-Bayes', 'KNN', 'Logistic Regression'))
     st.title('e-mail Spam Detection web app')
     EnterTheSubject=st.text_input('Subject')
     EnterTheBody=st.text_input('Body')
@@ -25,7 +33,8 @@ def main():
     
     #creating a button for prediction
     if st.button('Email detection'):
-        prediction=emailPrediction(EnterTheMail)
+        prediction=Prediction(EnterTheMail,option)
+
     st.success(prediction)
 
 if __name__ == "__main__":
